34332655b011b586794870de9ba3bb7905d5b831
Russell Johnson <russellmax86@gmail.com>
2016-03-08 14:45:55 -0700

    fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
-  get 'items/index'
-
-  get 'items/show'
-
-  get 'items/new'
-
-  get 'b_lists/index'
-
-  get 'b_lists/show'
-
-  get 'b_lists/new'
-
   root 'home#index'
 
-  devise_for :users
+  resources :b_lists do
+    resources :items
+  end
+
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 
 end
