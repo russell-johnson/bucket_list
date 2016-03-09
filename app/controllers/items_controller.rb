e880786dc417517f65aa1c6cc36c8b9b28be8cb4
Russell Johnson <russellmax86@gmail.com>
2016-03-08 21:53:49 -0700

    worked on edit functions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Items.all 
+    @items = Items.all
   end
 
   def show
@@ -12,8 +12,22 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-def create
-  b_list
+  def edit
+    @b_list = BList.find(params[:b_list_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(params[:item].permit(:name, :due_date, :location))
+      redirect_to b_list_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  def create
+    b_list
     @item = @b_list.items.create(item_params)
 
     if @item.save
@@ -32,7 +46,7 @@ def create
   end
 
   def item
-    @item = Item.new 
+    @item = Item.new
   end
   def item_params
     params.require(:item).permit(:name, :due_date, :location)
